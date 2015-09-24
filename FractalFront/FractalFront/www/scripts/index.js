16155bdc20a1274755d88dfc2c46a8687db440cb
Stefan <stefan.musterschueler@gmail.com>
2015-09-24 20:04:05 +0200

    First successful build

--- FractalFront/FractalFront/www/scripts/index.js
+++ FractalFront/FractalFront/www/scripts/index.js
@@ -1,17 +1,13 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
+﻿(function() {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener( "deviceready", onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener( "pause", onPause.bind( this ), false );
+        document.addEventListener( "resume", onResume.bind( this ), false );
+
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
@@ -22,4 +18,4 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
