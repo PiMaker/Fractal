f37c50f43f24498876dd876e5c047ed22ecd298c
PiMaker <stefan.musterschueler@gmail.com>
2015-10-07 12:21:50 +0200

    List and design improvements and feature adds

--- FractalFront/FractalFront/www/scripts/index.js
+++ FractalFront/FractalFront/www/scripts/index.js
@@ -1,6 +1,9 @@
 ﻿(function() {
     "use strict";
 
+    var randomModeText = "Random playlist, drag songs here to create your own";
+    $("#play-list").append("<li>" + randomModeText);
+
     document.addEventListener( "deviceready", onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
@@ -22,14 +25,49 @@
         for (var i in jsonData) {
             if (jsonData.hasOwnProperty(i)) {
                 var musicTitle = jsonData[i];
-                var text = musicTitle.ID3Tags.Title + " - " + musicTitle.ID3Tags.JoinedArtists;
-                $(musicList).append("<li>" + text).addClass("list-group-item");
+                var text = "ERROR";
+                if (musicTitle.ID3Tags.Title == undefined || musicTitle.ID3Tags.JoinedArtists == undefined) {
+                    text = musicTitle.Filename;
+                } else {
+                    text = musicTitle.ID3Tags.Title + " - " + musicTitle.ID3Tags.JoinedArtists;
+                }
+                $(musicList).append("<li><span class='handle'>☰</span>" + text).addClass("list-group-item");
             }
         }
 
-        // Create sortable list
+        // Create sortable lists
         Sortable.create(musicList, {
-            group: { name: "music-list-group", pull: "clone", put: false }
+            group: { name: "music-list-group", pull: "clone", put: false },
+            handle: ".handle",
+            scroll: true,
+            sort: false
+        });
+
+        var playListJQ = $("#play-list");
+        function updateRandomModeText() {
+            playListJQ.children().filter(function () {
+                return $(this).text() === randomModeText;
+            }).remove();
+            if (playListJQ.children().length === 0) {
+                playListJQ.append("<li>" + randomModeText);
+            }
+        }
+
+        Sortable.create(document.getElementById("play-list"), {
+            group: { name: "music-list-group", pull: false, put: true },
+            handle: ".handle",
+            scroll: true,
+            sort: true,
+            onSort: function(evt) {
+                updateRandomModeText();
+                var item = $(evt.item);
+                var removeBtn = item.append("<button>");
+                removeBtn.addClass("remove-btn");
+                removeBtn.on("click", function() {
+                    item.remove();
+                    updateRandomModeText();
+                });
+            }
         });
     };
 
